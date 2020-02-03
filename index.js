30e0f40e66ad39bd460dbc0f21474423932676d4
Micah Lynn <micah@sabado.com>
2020-02-03 16:08:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(katzDeliLine) {
     var currentLineString = "The line is currently: "
     var x = katzDeliLine.length
     for (var i=katzDeliLine.length; i>1 ; i--) {
-      currentLineString = currentLineString + (`${x-i + 1}. ${katzDeliLine[x-i]},`)
+      currentLineString = currentLineString + (` ${x-i + 1}. ${katzDeliLine[x-i]},`)
     }
     currentLineString = currentLineString + (`${x}. ${katzDeliLine[x-1]}`)
     return currentLineString
